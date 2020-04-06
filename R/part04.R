5c42d5d12212600026fd24daacaa6d4dc2480ccf
hamedbh <hamedbh@users.noreply.github.com>
2020-04-06 11:40:18 +0100

    finish model tuning section

--- R/part04.R
+++ R/part04.R
@@ -104,3 +104,120 @@ collect_metrics(easy_eval)
 collect_metrics(easy_eval, 
                 summarize = FALSE)
 
+
+# Model Tuning ------------------------------------------------------------
+
+
+# Cannot estimate some parameters from the data
+
+# Start using grid search. Elements required: 
+# 1. Set of candidate hyperparameter values to evaluate (the grid)
+# 2. A metric for model performance
+# 3. A resampling scheme to reliably estimate out-of-sample performance
+
+# Two main types of grid: regular and non-regular
+
+# Regular grids
+
+penalty()
+mixture()
+
+(glmn_param <- parameters(penalty(), mixture()))
+
+glmn_grid <- glmn_param %>% 
+    grid_regular(levels = c(10, 5)) #NB. different number for each
+
+glmn_grid %>% head()
+# The regular grid is on the transformed scale, then the values in the tibble 
+# are on the original scale
+
+# Non-regular grids are based on space-filling designs, keeping parameter 
+# combinations away from each other.
+
+set.seed(7454)
+
+glmn_sfd <- glmn_param %>% 
+    grid_max_entropy(size = 50)
+
+glmn_sfd %>% head()
+
+(glmn_set <- parameters(
+    lambda = penalty(), 
+    mixture()
+))
+
+(glmn_set <- glmn_set %>% 
+        update(lambda = penalty(c(-5, -1))))
+
+# in some cases the possible parameter values will depend on the data
+
+(rf_set <- parameters(
+    mtry(), 
+    trees()
+))
+
+finalize(rf_set, 
+         mtcars %>% 
+             dplyr::select(-mpg))
+
+?nearest_neighbor
+
+# Params are neighbors, weight_func, and dist_power
+knn_set <- parameters(neighbors(), 
+           dist_power())
+
+knn_grid <- knn_set %>% 
+    grid_regular(levels = c(10, 4)) %>% 
+    add_column(id = "Regular", 
+               .before = 1)
+
+knn_max_ent <- knn_set %>% 
+    grid_max_entropy(size = 40) %>% 
+    add_column(id = "Max Entropy", 
+               .before = 1)
+
+knn_latin <- knn_set %>% 
+    grid_latin_hypercube(size = 40) %>% 
+    add_column(id = "Latin Hypercube", 
+               .before = 1)
+
+bind_rows(knn_grid, knn_max_ent, knn_latin) %>% 
+    ggplot(aes(neighbors, dist_power)) + 
+    geom_point() + 
+    facet_wrap(~ id) + 
+    theme_light() + 
+    labs(
+        title = "Hyperparameter grids"
+    )
+
+# Tag parameters to be tuned
+knn_mod <- nearest_neighbor(
+    neighbors = tune(), 
+    weight_func = tune()
+) %>% 
+    set_engine("kknn") %>% 
+    set_mode("regression")
+
+parameters(knn_mod)
+
+set.seed(522)    
+
+knn_grid <- knn_mod %>% 
+    parameters() %>% 
+    grid_regular(levels = c(15, 5))
+
+ctrl <- control_grid(verbose = TRUE)
+
+knn_tune <- tune_grid(
+    knn_mod, 
+    ames_rec, 
+    resamples = cv_splits, 
+    grid = knn_grid, 
+    control = ctrl
+)
+
+knn_tune
+
+knn_tune$.metrics[[1]]
+
+show_best(knn_tune, "rmse")
