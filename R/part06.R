0124ff217c6f1df13eae89e7a48ef0db373b9edd
hamedbh <hamedbh@users.noreply.github.com>
2020-04-10 13:14:00 +0100

    stopped before downsampling exercise

--- R/part06.R
+++ R/part06.R
@@ -37,6 +37,7 @@ library(modeldata)
 data(small_fine_foods)
 
 library(textrecipes)
+library(textfeatures)
 
 # quick helper function
 count_to_binary <- function(x) {
@@ -44,7 +45,7 @@ count_to_binary <- function(x) {
 }
 
 text_rec <- recipe(score ~ product + review,
-       data = training_data) %>% 
+                   data = training_data) %>% 
     update_role(product, new_role = "id") %>% 
     step_mutate(review_raw = review) %>% # creates a temporary copy
     # next step creates some standards numeric features from the text, e.g. 
@@ -62,3 +63,92 @@ text_rec <- recipe(score ~ product + review,
 set.seed(8935)
 text_folds <- vfold_cv(training_data, strata = "score")
 
+
+# Classification Trees ----------------------------------------------------
+
+tree_rec <- recipe(score ~ product + review,
+                   data = training_data) %>% 
+    update_role(product, new_role = "id") %>% 
+    step_mutate(review_raw = review) %>% # creates a temporary copy
+    step_textfeature(review_raw) %>% 
+    step_tokenize(review) %>% 
+    step_stopwords(review) %>% 
+    step_stem(review) %>% 
+    step_texthash(review, 
+                  signed = FALSE, 
+                  # now number of hash terms is a tuning parameter
+                  num_terms = tune()) %>% 
+    step_zv(all_predictors())
+
+cart_mod <- decision_tree(cost_complexity = tune(), 
+                          min_n = tune()) %>% 
+    set_engine("rpart") %>% 
+    set_mode("classification")
+
+ctrl <- control_grid(save_pred = TRUE)
+
+cart_wfl <- workflow() %>% 
+    add_model(cart_mod) %>% 
+    add_recipe(tree_rec)
+
+set.seed(2553)
+
+# wrap the tuning function call in a conditional to read from disk if possible
+tune_path <- here::here("data", "cart_tune.rds")
+
+if (file.exists(tune_path)) {
+    cart_tune <- readr::read_rds(tune_path)
+} else {
+    cart_tune <- tune_grid(
+        cart_wfl, 
+        text_folds, 
+        grid = 10, 
+        metrics = metric_set(roc_auc), 
+        control = ctrl
+    )
+    readr::write_rds(cart_tune, tune_path)
+}
+show_best(cart_tune, "roc_auc")
+autoplot(cart_tune) + theme_light()
+
+cart_pred <- collect_predictions(cart_tune)
+
+cart_pred %>% 
+    inner_join(
+        select_best(cart_tune, "roc_auc"), 
+        by = c("num_terms", "cost_complexity", "min_n")
+    ) %>% 
+    group_by(id) %>% 
+    roc_curve(score, .pred_great) %>% 
+    autoplot()
+
+# Could pool the results across the folds and get something like an average
+add_curve_data <- function(x) {
+    collect_predictions(x) %>% 
+        inner_join(select_best(x, "roc_auc")) %>% 
+        roc_curve(score, .pred_great)
+}
+
+approx_roc_curves <- function(...) {
+    curves <- map_dfr(
+        list(...), 
+        add_curve_data, 
+        .id = "model"
+    )
+    
+    default_cut <- curves %>% 
+        group_by(model) %>% 
+        arrange(abs(.threshold - .5)) %>% 
+        slice(1)
+    ggplot(curves) +
+        aes(y = sensitivity, x = 1 - specificity, 
+            col = model) +
+        geom_abline(lty = 3) + 
+        geom_step(direction = "vh") + 
+        geom_point(data = default_cut) + 
+        coord_equal() + 
+        theme_light() + 
+        theme(legend.position = "top")
+}
+
+approx_roc_curves(CART = cart_tune)
